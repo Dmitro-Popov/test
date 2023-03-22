ff16c0a5fc4d42cfb4f2e1701aac8fe3253ef28f
Ivan <dimafax222@gmail.com>
2023-03-22 10:46:07 +0200

    lesson19

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 
 
-const user = 'Svan';
+let namberOfFilms = prompt('Сколько фильмов вы посмотрели');
+let washedFilms = prompt('Какой фильм Вам понравился?');
+let washedRating = prompt('На сколько оцените его?');
 
-alert(`Привет ${user}`);
+let personalMovieDB = {
+   count: namberOfFilms,
+   movies: [washedFilms, washedRating],
+   actors: {},
+   genres: [],
+   privat: false
+
+};
+
+
+personalMovieDB.movies[washedFilms];
+personalMovieDB.movies[washedRating];
+
+console.log(personalMovieDB);
