a53232fe815d8aefee32bbe4c04110a44622ccf1
Ivan <dimafax222@gmail.com>
2023-10-04 10:56:15 +0300

    Lesson54

--- work.js
+++ work.js
@@ -1,4 +1,17 @@
 const userData = {
    name: 'Ivan',
-   age: null
+   age: null,
+   say: function () {
+      console.log('Hello');
+   }
 }
+
+
+userData.say();
+userData.hay?.();
+// if (userData && userData.skills && userData.skills.js) {
+//    console.log(userData.skills.js);
+// }
+
+console.log(userData.skills?.js);
+
